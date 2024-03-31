9f16071b5f17dc8a943984af55c022f0b18615bb
fazbrainly <146303897+fazbrainly@users.noreply.github.com>
2024-03-31 16:15:07 +0700

    chore: added user agent in header

--- stats.ts
+++ stats.ts
@@ -100,6 +100,7 @@ const token = process.env.BR_TOKEN;
 const headers = {
   "Content-Type": "application/json",
   "Authorization": `Bearer ${token}`,
+  "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3",
 };
 
 axios.post("https://brainly.co.id/graphql/id", { query, variables: { query: keyword } }, { headers })
